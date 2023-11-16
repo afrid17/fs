0ea5103cc69da73607e4a766918133364c47d41a
AFRID SHAIK <afridayan01@gmail.com>
2023-11-16 06:18:06 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-
 export default function Home() {
   return (
     <>
-    afrid
+      <main>
+        <div>
+          <div>
+            {
+              /**
+               * 
+               */
+            }
+            <div className="p-6">
+              <h2 className="font-bold text-lg">Shop By Categories</h2>
+              <h4 className="text-lightGray text-sm">Freshest Meats and Much More!</h4>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss'
-
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +6,35 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily:{
+      nunitoSans: ['Nunito Sans', 'sans-serif'],
+      poppins: ['Poppins', 'sans-serif'],
+      robotoMono: ['Roboto Mono', 'monospace'],
+      roboto: ['Roboto', 'sans-serif'],
+      mont:['Montserrat','sans-serief'],
+      openSans:['Open Sans','sans-serief'],
+      ubuntu:['Ubuntu','sans-serief']
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        'sm': '300px',
+        'md': '600px',
+        'lg': '1023px',
       },
+      colors:{
+        lightGray:"#6c757d",
+        black:"#000000",
+        yellow:"#ffd60a",
+        darkOrange:"#fb5607",
+        darkBlue:"#03045e",
+        blueShade:"#caf0f8",
+        white:"#ffffff",
+        redPink:"#ff0054",
+        pinkShade:"#ffeef2",
+        darkPurple:"#3c096c",
+        purple:"#7b2cbf",
+        green:"#5cf64a",
+      }
     },
   },
   plugins: [],
