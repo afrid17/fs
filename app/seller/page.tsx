45e426f4d3951adf5741118f0b3ed981f989c5de
afrid17 <145343404+afrid17@users.noreply.github.com>
2023-12-21 05:08:57 +0000

    update

--- app/seller/page.tsx
+++ app/seller/page.tsx
@@ -8,7 +8,13 @@ export default function Seller(){
     return(
         <>
             <main>
-
+                <section>
+                    <div>
+                        <div>
+                            <img  src="images/sellerLoginLogo.jpg" />
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     )
